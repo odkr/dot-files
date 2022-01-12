d1749a5f37453b29bc9494631ead171a9c31a6f0
Odin Kroeger <odkr@users.noreply.github.com>
2022-01-12 10:59:48 +0100

    fix: fixed installer.

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ set -eu
 
 : "${HOME:?}"
 
+SCRIPT_NAME="$(basename "$0")" && [ "$SCRIPT_NAME" ] || {
+	printf '%s: failed to determine my filename.\n' "$0" >&2
+	exit 69
+}
+
 SCRIPT_DIR="$(dirname "$0")" && [ "$SCRIPT_DIR" ] || {
 	printf '%s: failed to locate myself.\n' "$0" >&2
 	exit 69
@@ -13,4 +18,4 @@ cd -P "$SCRIPT_DIR" || exit 69
 
 git pull
 
-find . ! -name .git -execdir cp -av '{}' + "$HOME/"
+find . ! -name .git -a ! -name "$SCRIPT_NAME" -execdir cp -av '{}' + "$HOME/"
